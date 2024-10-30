bc3893e3cf600bb3359f413c4bb9948806ca9d7f
pou0119 <yuuyiteng0@gmail.com>
2024-10-30 11:29:10 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
-  def new
-    @user=User.new
-  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 
   private
